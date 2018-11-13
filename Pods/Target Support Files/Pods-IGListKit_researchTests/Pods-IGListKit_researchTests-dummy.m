2a811b36d3823717811b3feb52aace2322ad8727
Sergey Lamcev <sergey.lamcev@anoda.mobi>
2018-11-13 16:32:34 +0200

    Added gitignore

--- Pods/Target Support Files/Pods-IGListKit_researchTests/Pods-IGListKit_researchTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IGListKit_researchTests : NSObject
-@end
-@implementation PodsDummy_Pods_IGListKit_researchTests
-@end
--- Pods/Target Support Files/Pods-IGListKit_researchUITests/Pods-IGListKit_researchUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IGListKit_researchUITests : NSObject
-@end
-@implementation PodsDummy_Pods_IGListKit_researchUITests
-@end
